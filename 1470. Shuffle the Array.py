131ed42b9837ec5f782b4d1a5825555111a2d4ba
DEEPAK KUMAR <113495226+deepak956883@users.noreply.github.com>
2023-02-06 14:49:02 +0530

    Add files via upload

---
+++ 1470. Shuffle the Array.py
@@ -0,0 +1,23 @@
+'''
+1470. Shuffle the Array
+
+'''
+
+
+
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        i = 0
+        j = n
+        ans = []
+        while (i<n and j<2*n):
+            for k in range(2*n):
+                if k%2==0:
+                    ans.append(nums[i])
+                    i = i+1
+                else:
+                    ans.append(nums[j])
+                    j = j+1
+        return ans 
