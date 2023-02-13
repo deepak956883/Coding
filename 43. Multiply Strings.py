abad9ede88bfc6943de9ed688309e6fedc753855
DEEPAK KUMAR <113495226+deepak956883@users.noreply.github.com>
2023-02-13 22:28:05 +0530

    Add files via upload

---
+++ 43. Multiply Strings.py
@@ -0,0 +1,12 @@
+'''
+43. Multiply Strings
+
+'''
+
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        a = int(num1)
+        b = int(num2)
+        c = str(a*b)
+        return c
