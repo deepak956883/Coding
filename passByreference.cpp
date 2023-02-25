7221afb2ca8644ef797cd59437620bf692416687
DEEPAK KUMAR <113495226+deepak956883@users.noreply.github.com>
2023-02-25 14:54:56 +0530

    Add files via upload

---
+++ passByreference.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void fun(int &x, int &y){
+    int temp ;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+int main(){
+    int a = 5 , b = 10;
+    fun(a,b);
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b;
+    return 0;
+}
